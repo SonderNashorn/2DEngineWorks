c7ac7680a7538041eb7b5a32d044d619d113a04a
SonderNashorn <SonderNashorn@users.noreply.github.com>
2018-10-03 12:00:26 +0100

    Basic setup Update

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			PApplet.main("Main");
+	}
+	public void setup ()
+	{
+		background(0);
+		
+		
+	}
+	public void draw()
+	{
+		
+		
+	}
+	public void settings()
+	{
+		size(640,480);
+		
+	}
+}
